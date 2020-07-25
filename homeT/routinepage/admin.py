7ee9737a7993559f87c4ee42824f563950ff056b
JeongJiyeon <wldusdhso@gmail.com>
2020-07-26 02:59:30 +0900

    template

--- homeT/routinepage/admin.py
+++ homeT/routinepage/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Routine
+from .models import Comment
 # Register your models here.
+
+admin.site.register(Routine)
+admin.site.register(Comment)
