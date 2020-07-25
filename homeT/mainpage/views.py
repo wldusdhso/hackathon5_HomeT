dec9d4306e17bf888ce662bc9c1fcb5685292839
JeongJiyeon <wldusdhso@gmail.com>
2020-07-26 04:32:23 +0900

    jieyon

--- homeT/mainpage/views.py
+++ homeT/mainpage/views.py
@@ -4,8 +4,6 @@ from routinepage.models import Routine
 # Create your views here.
 def main(request):
     hotRoutine = Routine.objects.all().order_by('-id')[:5]
-
-
     return render(request, 'main.html',{
         'hotRoutine' : hotRoutine,
     })
