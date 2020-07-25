1bfb307a6e6000c1bb0249334abc968b09bb2d48
JeongJiyeon <wldusdhso@gmail.com>
2020-07-26 00:49:42 +0900

    merge main&account

--- homeT/account/views.py
+++ homeT/account/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import SigninForm
-from django.contrib import auth
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate,logout, get_user_model
 from django.http import HttpResponse
 
 # Create your views here.
@@ -32,8 +30,11 @@ def signup(request):
         if request.POST['password1'] == request.POST['password2']:
             user = User.objects.create_user(
                 username=request.POST['username'], password=request.POST['password1'])
-            auth.login(request, user)
+            login(request, user)
             return redirect('mainpage:main')
     else:
         return render(request, 'account/signup.html')
 
+def signout(request):
+    logout(request)
+    return redirect("mainpage:main")
